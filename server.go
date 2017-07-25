9ea4afbaf8dc43053b1dfbdac5a83080d1eac622
Oliver Guggenbuehl <o.guggenbuehl@gmail.com>
2017-07-25 11:11:00 +0200

    updateded echo

--- server.go
+++ server.go
@@ -6,8 +6,17 @@ import (
 	"github.com/labstack/echo"
 )
 
+// e.POST("/save", save)
+func save(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:"+name+", email:"+email)
+}
+
 func main() {
 	e := echo.New()
+	e.POST("/save", save)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
